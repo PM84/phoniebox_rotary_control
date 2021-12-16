3140dc0ed3f62670f13a3d745dafadc3c16cc2fb
Olaf Splitt <olaf.splitt@gmail.com>
2021-12-16 23:29:30 +0100

    1.0.4

--- scripts/install/prc_installer.sh
+++ scripts/install/prc_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Colors: \e[36m=Cyan M ; \e[92m=Light green ; \e[91m=Light red ; \e[93m=Light yellow ; \e[31m=green ; \e[0m=Default ; \e[33m=Yellow ; \e[31m=Red
 
-#Version: 1.0.3 - 20211216
+#Version: 1.0.4 - 20211216
 #branch="development"
 repo="https://github.com/splitti/phoniebox_rotary_control"
 branch="main"
@@ -85,7 +85,7 @@ echo -e "${green}Done${nocolor}"
 echo -e ""
 echo -e -n "   --> Clone phoniebox_rotary_control Repository:   "
 
-git clone ${repo} --branch ${branch} ${installPath} # > /dev/null 2>&1
+git clone ${repo} --branch ${branch} ${installPath} > /dev/null 2>&1
 echo -e "${green}Done${nocolor}"
 echo -e ""
 echo -e -n "   --> Installing Service:                "
@@ -95,9 +95,9 @@ sudo cp ${installPath}/templates/service.template /etc/systemd/phoniebox_rotary_
 sudo chown root:root /etc/systemd/phoniebox_rotary_control.service > /dev/null 2>&1
 sudo chmod 644 /etc/systemd/phoniebox_rotary_control.service > /dev/null 2>&1
 sudo sed -i -e "s:<PATH>:${installPath}:g" /etc/systemd/phoniebox_rotary_control.service > /dev/null
-sudo systemctl daemon-reload > /dev/null 2>&1
-sudo systemctl enable /etc/systemd/phoniebox_rotary_control.service > /dev/null 2>&1
-sudo service phoniebox_rotary_control restart > /dev/null 2>&1
+sudo systemctl daemon-reload #> /dev/null 2>&1
+sudo systemctl enable /etc/systemd/phoniebox_rotary_control.service #> /dev/null 2>&1
+sudo service phoniebox_rotary_control restart #> /dev/null 2>&1
 echo -e "${green}Done${nocolor}"
 echo -e ""
 echo -e ""
