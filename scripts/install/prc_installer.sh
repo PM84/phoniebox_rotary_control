ddf0d0ea2670e336c8144f4de8db6952dd2d5db2
Olaf Splitt <olaf.splitt@gmail.com>
2021-12-17 22:48:58 +0100

    Update prc_installer.sh

--- scripts/install/prc_installer.sh
+++ scripts/install/prc_installer.sh
@@ -60,7 +60,7 @@ do
             ;;
 
         "Remove")
-		    inst_type="r"
+		    insttype="r"
             break
             ;;
 
@@ -90,15 +90,108 @@ sudo rm -R ${installPath} > /dev/null 2>&1
 echo -e "${green}Done${nocolor}"
 echo -e ""
 
-echo "Insttype: ${insttype} ---"
-
 if [ "$insttype" = "i" ]
 then
-    read -n 1 -s -r -p "Press any key to continue"
+    read -n 1 -s -r -p "Service removed. Press any key to continue"
 else
 	echo -e "${green}Service successfully removed...${nocolor}"
 	exit
 fi
+
+clear
+cd
+echo -e "////////////////////////////////////////////////////////////////////"
+echo -e "///${cyan}   Check/Install Prerequirements:                             ${nocolor}///"
+echo -e "////////////////////////////////////////////////////////////////////"
+echo -e " "
+echo -e "Do you want to start the installation of needed packages?"
+echo -e " "
+options=("Install" "Quit")
+
+select opt in "${options[@]}"
+do
+    case $opt in
+        "Install")
+            break
+            ;;
+
+        "Quit")
+            exit
+            ;;
+        *) echo -e "invalid option $REPLY";;
+    esac
+done
+echo -e ""
+echo -e "Starting installation-process, pleae wait, some"
+echo -e "installation steps take some time..."
+echo -e ""
+echo -e -n "   --> Update Sources:          "
+sudo apt -qq update > /dev/null 2>&1
+echo -e "${green}done${nocolor}"
+echo -e ""
+echo -e "Install packages..."
+
+lineLen=24
+packages=(git python3) 
+for p in ${packages[@]}; do
+	i=0
+	echo -n -e "   --> $p:"
+    let lLen="$lineLen"-"${#p}"
+    while [ "$i" -lt "$lLen" ]
+    do
+		let i+=1
+		echo -n -e " "
+	done
+    installer=`sudo dpkg -s ${p}  2>&1 | grep Status | grep installed`
+    if [ "$installer" = "" ]
+    then
+		installer=`sudo apt -qq -y install ${p} > /dev/null 2>&1`
+		installer=`sudo dpkg -s ${p} 2>&1 | grep Status | grep installed`
+		if [ "$installer" = "" ]
+		then
+			echo -e "${red}failed${nocolor}"
+		else
+			echo -e "${green}done${nocolor}"
+		fi
+	else
+		echo -e "${green}already installed${nocolor}"
+	fi
+done
+lumaPackages=(evdev)
+for p in ${lumaPackages[@]}; do
+	i=0
+	let lLen="$lineLen"-"${#p}"
+	echo -n -e "   --> $p:"
+	while [ "$i" -lt "$lLen" ]
+	do
+		let i+=1
+		echo -n -e " "
+	done
+	pipInstalled=`sudo pip3 show ${p}`
+	if [ "$pipInstalled" = "" ]
+	then
+		sudo pip3 install ${p}  > /dev/null 2>&1
+		pipInstalled=`sudo pip3 show ${p}`
+		if [ "$pipInstalled" = "" ]
+		then
+			echo -e "${red}failed${nocolor}"
+		else
+			echo -e "${green}done${nocolor}"
+		fi
+	else
+		echo -e "${green}already installed${nocolor}"
+	fi
+done
+
+echo -e -n "   --> Adding config-entries:             "
+echo 'dtoverlay=rotary-encoder,pin_a=23,pin_b=24,relative_axis=1' | sudo tee -a /boot/config.txt > /dev/null 2>&1
+echo 'dtoverlay=gpio-key,gpio=22,keycode=28,label="ENTER"' | sudo tee -a /boot/config.txt > /dev/null 2>&1
+echo -e "${green}Done${nocolor}"
+
+echo -e ""
+read -n 1 -s -r -p "Press any key to continue"
+
+
 
 clear
 echo -e "///////////////////////////////////////////////////////////////////////////"
@@ -109,23 +202,7 @@ echo -e "Repository:       ${green}${repo}${nocolor}"
 echo -e "Branch:           ${green}${branch}${nocolor}"
 echo -e "Install Path:     ${green}${installPath}${nocolor}"
 echo -e ""
-echo -e -n "   --> Delete old Service:                "
-sudo service phoniebox_rotary_control stop > /dev/null 2>&1
-sudo systemctl disable /etc/systemd/phoniebox_rotary_control.service > /dev/null 2>&1
-sudo rm /etc/systemd/phoniebox_rotary_control.service > /dev/null 2>&1
-echo -e "${green}Done${nocolor}"
-echo -e -n "   --> Remove config-entries:             "
-sudo sed -i '/dtoverlay=rotary-encoder/c\' /boot/config.txt
-sudo sed -i '/dtoverlay=gpio-key/c\' /boot/config.txt
-echo -e "${green}Done${nocolor}"
-echo -e -n "   --> Delete old Repository:             "
-sudo rm -R ${installPath} > /dev/null 2>&1
-echo -e "${green}Done${nocolor}"
-echo -e ""
-echo -e -n "   --> Adding config-entries:             "
-echo 'dtoverlay=rotary-encoder,pin_a=23,pin_b=24,relative_axis=1' | sudo tee -a /boot/config.txt > /dev/null 2>&1
-echo 'dtoverlay=gpio-key,gpio=22,keycode=28,label="ENTER"' | sudo tee -a /boot/config.txt > /dev/null 2>&1
-echo -e "${green}Done${nocolor}"
+echo -e -n "   --> Install:             "
 echo -e -n "   --> Clone Rotary Repository:           "
 git clone ${repo} --branch ${branch} ${installPath} > /dev/null 2>&1
 echo -e "${green}Done${nocolor}"
