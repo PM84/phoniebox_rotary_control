bdec8c28a1ea61530a87735bb79ea737c0546e3e
Olaf Splitt <olaf.splitt@gmail.com>
2021-12-18 00:09:21 +0100

    Update prc_installer.sh

--- scripts/install/prc_installer.sh
+++ scripts/install/prc_installer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Colors: \e[36m=Cyan M ; \e[92m=Light green ; \e[91m=Light red ; \e[93m=Light yellow ; \e[31m=green ; \e[0m=Default ; \e[33m=Yellow ; \e[31m=Red
 
-#Version: 1.1.12 - 20211218
+#Version: 1.1.13 - 20211218
 #branch="development"
 repo="https://github.com/splitti/phoniebox_rotary_control"
 branch="main"
@@ -244,8 +244,10 @@ do
 			elif
 				echo -e "${green}Service running with PID ${pid}${nocolor}"
 		    fi
+            echo -e " "
 		    break
             ;;
+
         "Quit")
             exit
             ;;
