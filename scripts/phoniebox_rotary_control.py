0718339558093ecf58c3982dbaf716298e0ac4db
Olaf Splitt <olaf.splitt@gmail.com>
2021-12-17 02:08:39 +0100

    1.0.25

--- scripts/phoniebox_rotary_control.py
+++ scripts/phoniebox_rotary_control.py
@@ -2,14 +2,24 @@
 # -*- coding: iso-8859-15 -*-
 # 1.0.24
 
-import RPi.GPIO as GPIO
-from pyky040 import pyky040
+#!/usr/bin/env python
+
+import os
+from RPi import GPIO
+from KY040 import KY040
 import os, time
- 
- 
+
 def readVolume():
-    value = int(os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getvolume").read())
+    value = os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getvolume").read()
     return int(value)
+def getBootVolume():
+    bootvol = os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=getbootvolume").read()
+    return int(bootvol)
+def checkIfMuted():
+    if readVolume() > 1: 
+        return False
+    else:
+        return True
 def rotaryChange(direction):
     volume_step = 5
     volume = readVolume()
@@ -17,26 +27,28 @@ def rotaryChange(direction):
         os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(min(100,max(0,volume + volume_step))))
     else:
         os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(min(100,max(0,volume - volume_step))))
-def switchPressed():
-    os.system("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=mute")
- 
+def switchPressed(pos):
+    if checkIfMuted() == False:
+        os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=mute")
+    else:
+        bootvol = getBootVolume()
+        os.popen("sudo /home/pi/RPi-Jukebox-RFID/scripts/playout_controls.sh -c=setvolume -v="+str(bootvol)) 
  
 if __name__ == "__main__":
  
     CLOCKPIN = 5
     DATAPIN = 6
     SWITCHPIN = 13
-  
-    
+
     GPIO.setmode(GPIO.BCM)
     
-    pyky040 = pyky040(CLOCKPIN, DATAPIN, SWITCHPIN, rotaryChange, switchPressed)
+    ky040 = KY040(CLOCKPIN, DATAPIN, SWITCHPIN, rotaryChange, switchPressed)
  
-    pyky040.start()
+    ky040.start()
  
     try:
         while True:
             time.sleep(0.05)
     finally:
-        pyky040.stop()
+        ky040.stop()
         GPIO.cleanup()
